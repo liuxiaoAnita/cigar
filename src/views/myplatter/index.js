0d6a8f88b3ee1c34afbba206cebee5ef78ecfd7a
xiaoxiao <779275936@qq.com>
2020-11-07 20:33:46 +0800

    feat：自选拼盘

--- src/views/myplatter/index.js
+++ src/views/myplatter/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, Icon } from "antd";
+import { Button, Icon, message } from "antd";
+import { login } from "@/store/actions";
 import BannerAdvert from "@/components/Banner/advert.js";
 import TitleSplit from "@/components/TitleSplit/index.js";
 
@@ -7,12 +8,36 @@ import TitleSplit from "@/components/TitleSplit/index.js";
 import "./index.less";
 class MyPlatterPage extends Component {
   state = {
+    loading: true,
     users: [],
     myOrder: [],
     totleCount: 0,
+    dataList: []
   };
   componentDidMount() {
     // this.getUsers()
+    this.getZxppList()
+  }
+
+  getZxppList = () => {
+    const categoryId = localStorage.getItem('categoryId')
+    const params = {cmd: 'getZxppList', categoryId}
+    login(params)()
+      .then((res) => {
+        if (`${res.result}` === '0') {
+          console.log(res.body.dataList)
+          this.setState({
+            dataList: res.body.dataList || []
+          })
+        } else {
+          message.error(`${res.resultNote}`);
+        }
+        this.setState({loading: false})
+      })
+      .catch((error) => {
+        this.setState({loading: false})
+        message.error(error);
+      });
   }
 
   handelChangeNum = (val, type = true) => {
@@ -44,39 +69,21 @@ class MyPlatterPage extends Component {
   }
 
   renderPlatter = () => {
-    const platter = {// 精品商品列表
-      "id":"1",// 商品id
-      "zh_name": '比雅达 蜜饯', //中文名
-      "en_name": 'Jose L. Piedra Con', //英文名
-      "baozhuang_en_name": '保利华 比利高', //包装英文名
-      "baozhuang_zh_name": '保利华 比利高', //包装中文名
-      "price": '10.00', //会员价
-      "changdu": '127', //长度
-      "huanjing": '50', //环径
-      "old_price": '100.00', //市场价
-      "image": '', //图片
-    }
-    const platterData = [
-      {...platter, id: '1'},
-      {...platter, id: '2'},
-      {...platter, id: '3'},
-      {...platter, id: '4'},
-      {...platter, id: '5'},
-    ];
+    const {dataList} = this.state
     return (
       <div className='my-platter-content'>
-      {platterData.map((item, index) => (
+      {dataList.map((item, index) => (
         <div className='my-platter-item' key={`platter-item-${index}`}>
-          <img className='platter-image' src='' />
+          <img className='platter-image' src={item.image} />
           <div className='title-name'>
             <span>产品名称</span>
             <span>数量</span>
           </div>
           <div className='platter-mes'>
             <div className='message'>
-              <span className='name'>{item.zh_name}{item.en_name}</span>
+              <span className='name'>{item.zh_name || item.en_name}</span>
               <span className='baozhuang'>{item.baozhuang_zh_name}</span>
-              <span className='guige'>长度（毫米）{item.changdu}&nbsp;&nbsp;|&nbsp;&nbsp;环径 ${item.huanjing}</span>
+              <span className='guige'>长度（毫米）{item.changdu}&nbsp;&nbsp;|&nbsp;&nbsp;环径 {item.huanjing}</span>
             </div>
             <div className='action-box'>
               <span className='now-price'>￥{item.price}</span>
@@ -103,20 +110,22 @@ class MyPlatterPage extends Component {
       <div className='right-menu'>
         <div className='list-menu-box'>
           {myOrder.map((item, index) => (
-            <>{item.count > 0 && <div key={`my-order-right-${index}`} className='list-item'>
-              <span className='name'>{item.zh_name}</span>
-              <span className='count-number'>×{item.count}</span>
-              <div className='btn-box'>
-                <i className='minus' onClick={() => this.handelChangeNum(item, false)}>-</i>
-                <i className='plus' onClick={() => this.handelChangeNum(item)}>+</i>
-              </div>
-            </div>}</>
+            <React.Fragment key={`my-order-right-${index}`} >
+              {item.count > 0 && <div className='list-item'>
+                <span className='name'>{item.zh_name}</span>
+                <span className='count-number'>×{item.count}</span>
+                <div className='btn-box'>
+                  <i className='minus' onClick={() => this.handelChangeNum(item, false)}>-</i>
+                  <i className='plus' onClick={() => this.handelChangeNum(item)}>+</i>
+                </div>
+              </div>}
+            </React.Fragment>
           ))}
         </div>
         <div className='totle-price'>订单金额：{totleCount}</div>
         <div className='add-shop-action'>
-          <Button className='add-shop-btn' onClick={this.handelAddShop} type='primary'>添加到购物车</Button>
-          <Button className='add-heart-btn' onClick={this.handelAddHeart} type="link" block>
+          <Button className='add-shop-btn' onClick={() => this.handelAddShop('addCart')} type='primary'>添加到购物车</Button>
+          <Button className='add-heart-btn' onClick={() => this.handelAddShop('addXinyuandan')} type="link" block>
             <Icon className='icon-image' type="heart" />添加到心愿单
           </Button>
         </div>
@@ -124,9 +133,39 @@ class MyPlatterPage extends Component {
     )
   }
 
-  handelAddShop = () => {
-    console.log(this.state.myOrder)
+  handelAddShop = (type) => {
+    const uid = localStorage.getItem('userUid') || ''
+    if (uid === '') {
+      message.error('先登录！')
+    } else{
+      const {myOrder = []} = this.state
+      if (myOrder.length < 0) {
+        message.error('未添加内容')
 
+      } else {
+        const productList = []
+        myOrder.forEach(item => {
+          productList.push({
+            id: item.id,
+            count: item.count
+          })
+        })
+        login({cmd: type, uid, productList})()
+          .then((res) => {
+            if (`${res.result}` === '0') {
+              message.success(`添加${type === 'addCart' ? '购物城' : '心愿单'}成功！`)
+             
+            } else {
+              message.error(`${res.resultNote}`);
+            }
+            this.setState({loading: false})
+          })
+          .catch((error) => {
+            this.setState({loading: false})
+            message.error(error);
+          });
+      }
+    }
   }
 
   handelAddHeart = () => {
